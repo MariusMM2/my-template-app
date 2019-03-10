8f44b86b492c3049fec0b5fb9b9ccd897163c0c8
marius <cepulameavrei>
2019-03-10 12:16:42 +0100

    small theme updates

--- app/src/main/java/com/marius/mytemplate/activities/MainActivity.java
+++ app/src/main/java/com/marius/mytemplate/activities/MainActivity.java
@@ -1,14 +1,6 @@
 package com.marius.mytemplate.activities;
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.view.Menu;
-import android.view.MenuInflater;
-import com.mahfa.dnswitch.DayNightSwitch;
-import com.mahfa.dnswitch.DayNightSwitchAnimListener;
-import com.mahfa.dnswitch.DayNightSwitchListener;
-import com.marius.mytemplate.R;
-import com.marius.mytemplate.Settings;
 import com.marius.mytemplate.fragments.MainFragment;
 
 public class MainActivity extends SingleFragmentActivity {
@@ -16,44 +8,5 @@ public class MainActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new MainFragment();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.daynight_menu, menu);
-
-        DayNightSwitch dayNightSwitch = menu.findItem(R.id.day_night_switch).getActionView().findViewById(R.id.switch_item);
-        dayNightSwitch.setIsNight(Settings.getInstance(this).darkMode);
-        dayNightSwitch.setAnimListener(new DayNightSwitchAnimListener() {
-            @Override
-            public void onAnimStart() {
-
-            }
-
-            @Override
-            public void onAnimEnd() {
-                Fragment frag = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-                assert frag != null;
-                frag.onStop();
-                Intent intent = new Intent(frag.getActivity(), MainActivity.class);
-
-                startActivity(intent);
-                finish();
-            }
-
-            @Override
-            public void onAnimValueChanged(float v) {
-
-            }
-        });
-        dayNightSwitch.setListener(new DayNightSwitchListener() {
-            @Override
-            public void onSwitch(boolean isNight) {
-                Settings.getInstance(MainActivity.this).darkMode = isNight;
-            }
-        });
-
-        return true;
     }
 }
---
+++ app/src/main/java/com/marius/mytemplate/activities/ThemedSwitchActivity.java
@@ -0,0 +1,76 @@
+package com.marius.mytemplate.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import com.mahfa.dnswitch.DayNightSwitch;
+import com.mahfa.dnswitch.DayNightSwitchAnimListener;
+import com.marius.mytemplate.R;
+import com.marius.mytemplate.Settings;
+
+//An Activity with a day/night switch in the menu
+@SuppressLint("Registered")
+public class ThemedSwitchActivity extends AppCompatActivity {
+    @SuppressWarnings("unused")
+    private static final String TAG = ThemedSwitchActivity.class.getSimpleName();
+    private boolean mDarkMode;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        mDarkMode = Settings.getInstance(this).darkMode;
+        setTheme(mDarkMode ? R.style.AppNightTheme : R.style.AppDayTheme);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Settings.getInstance(this).save(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mDarkMode != Settings.getInstance(this).darkMode) {
+            recreate();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.daynight_menu, menu);
+
+        DayNightSwitch dayNightSwitch = menu.findItem(R.id.day_night_switch).getActionView().findViewById(R.id.switch_item);
+        dayNightSwitch.setIsNight(mDarkMode);
+        dayNightSwitch.setAnimListener(new DayNightSwitchAnimListener() {
+            @Override
+            public void onAnimStart() {
+
+            }
+
+            @Override
+            public void onAnimEnd() {
+                Intent intent = new Intent(ThemedSwitchActivity.this, ThemedSwitchActivity.this.getClass());
+                intent.putExtras(getIntent());
+
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+                finish();
+            }
+
+            @Override
+            public void onAnimValueChanged(float v) {
+
+            }
+        });
+        dayNightSwitch.setListener(isNight -> Settings.getInstance(this).darkMode = isNight);
+
+        return true;
+    }
+}
